080c4044da268d778ff6e708975c207e7d8a6b48
Praveen-kumar-developer <golubaghel800@gmail.com>
2024-12-16 20:26:33 +0530

    this file going to remote

--- activeNavLinks.js
+++ activeNavLinks.js
@@ -1,102 +1,16 @@
-// let sections = document.querySelectorAll("section");
-// let navlinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
-// window.onscroll = () => {
-//   sections.forEach((sec) => {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 150;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute('id');
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-//     if (top >= offset && top < offset + height) {
-//       navlinks.forEach((links) => {
-//         links.classList.remove("active");
-//         document.querySelector('header nav a[href*=' + id + ']').classList.add
-//         ('active');
-//       });  }  }); };
-
-/////////////////////////////////////
-
-// const sections2 = document.querySelectorAll("section");
-// const navlinks2 = document.querySelectorAll("header nav a");
-// window.onscroll = () => {
-//   sections2.forEach((sec2) => {
-//     let top = window.scrollY;
-//     let offset = sec2.offsetTop - 150;
-//     let height = sec2.offsetHeight;
-//     let id = sec2.getAttribute("id");
-//     if (top > offset && top < offset + height) {
-//       navlinks2.forEach((links2) => {
-//         links2.classList.remove("active");
-//         document
-//           .querySelector("header nav a[href*=" + id + "]")
-//           .classList.add("active");
-//       });
-//     }
-//   });
-// };
-
-/////////////////////////////////////////////////////
-
-// const secti = document.querySelectorAll("section");
-// const navli = document.querySelectorAll("header nav a");
-
-// window.onscroll = () => {
-//   secti.forEach((secccc) => {
-//     let top = window.scrollY;
-//     let offset = secccc.offsetTop - 150;
-//     let height = secccc.offsetHeight;
-//     let id = secccc.getAttribute("id");
-//     if (top > offset && top < offset + height) {
-//       navli.forEach((linkssss) => {
-//         linkssss.classList.remove("active");
-//         document
-//           .querySelector('header nav a[href*=' + id + ']')
-//           .classList.add('active');
-//       });
-//     }
-//   });
-// };
-
-// '''''''''''''''''''''''''''''''''''''''''''''''
-
-// const sections = document.querySelectorAll("section");
-// const navlinks = document.querySelectorAll("header nav a");
-
-// window.onscroll = () => {
-//   sections.forEach((sectionss) => {
-//     let top = window.scrollY;
-//     let offset = sectionss.offsetTop - 150;
-//     let height = sectionss.offsetHeight;
-//     let id = sectionss.getAttribute("id");
-
-//     if (top > offset && top < offset + height) {
-//       navlinks.forEach((links)=>{
-//         links.classList.remove('active')
-//         document.querySelector('header nav a[href*='+  id +']').classList.add('active')
-//       })
-//     }
-//   });
-// };
-
-
-/////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector('header nav a[href*=' + id + ']').classList.add
+        ('active');
+      });  }  }); };
